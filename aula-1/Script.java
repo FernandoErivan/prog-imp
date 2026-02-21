19093db0742379c298baf4aed37ad55833a3e750
FernandoErivan <fernandoo.estudos@gmail.com>
2026-02-20 21:28:08 -0300

    If else para validar idade

--- aula-1/Script.java
+++ aula-1/Script.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Script{
+public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.println("Olá, turma!");
+        System.out.println("Digite a sua idade: ");
+        int Idade = sc.nextInt();
+        if(Idade >= 18){
+            System.out.println("Pode tirar a carteira de motorista!");
+        }
+        else{
+            System.out.println("Você ainda não pode tirar a carteira de motorista!");
+        }
+        if(Idade < 16)
+        {
+            System.out.println("Você não pode votar!");
+        }
+        else if(Idade >= 16 && Idade < 18){
+             System.out.println("Seu voto é opcional!");
+        }
+        else if(Idade >= 18 && Idade < 70){
+            System.out.println("Seu voto é obrigatório!");
+        }
+        else{
+            ("Você não tem mais obrigação em votar!");
+        }
+
     }
 }
