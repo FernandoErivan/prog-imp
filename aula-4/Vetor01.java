cf8776894c04039e937729111317e6112489f367
FernandoErivan <fernandoo.estudos@gmail.com>
2026-02-27 21:07:22 -0300

    Vetores

---
+++ aula-4/Vetor01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Vetor01{
+public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+int[] idades = {34,45,65,86,25,21};
+
+System.out.println(idades);
+    }
+}
