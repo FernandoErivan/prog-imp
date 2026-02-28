bd19ee0ccc41ae411756b772015c7cf2673c65a8
FernandoErivan <fernandoo.estudos@gmail.com>
2026-02-27 21:35:35 -0300

    Vetores começo

--- aula-4/Vetor01.java
+++ aula-4/Vetor01.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 public class Vetor01{
-public static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args){
-int[] idades = {34,45,65,86,25,21};
+    public static Scanner sc = new Scanner(System.in);
+     public static final int TAM_MIN = 1;
+     public static final int TAM_MAX = 30;
+        public static void main(String[] args){
+        int QtdPessoas;
+        int[] idades;
+
+        do{
+            System.oit.printf("Digite a quantidade de pessoas (%id-%id): \n",TAM_MIN, TAM_MAX);
+            QtdPessoas = sc.nextInt();
+            
+        }
+
 
 System.out.println(idades);
     }
