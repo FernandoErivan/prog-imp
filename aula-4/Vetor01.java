9fdfcedc852b0767d1eb757f22ce57f036d8dc02
FernandoErivan <fernandoo.estudos@gmail.com>
2026-03-03 21:28:59 -0300

    Iniciação da atv

--- aula-4/Vetor01.java
+++ aula-4/Vetor01.java
@@ -3,10 +3,9 @@ public class Vetor01{
     public static Scanner sc = new Scanner(System.in);
      public static final int TAM_MIN = 1;
      public static final int TAM_MAX = 30;
+      int QtdPessoas;
+       int[] idades;
         public static void main(String[] args){
-        int QtdPessoas;
-        int[] idades;
-
         do{
             System.oit.printf("Digite a quantidade de pessoas (%id-%id): \n",TAM_MIN, TAM_MAX);
             QtdPessoas = sc.nextInt();
